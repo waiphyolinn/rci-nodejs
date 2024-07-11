30a1d0c93bccc2c0f6c1068d423415cce1f4d1fc
Wai Phyo Linn <phyolinn.wai@gmail.com>
2024-07-11 11:27:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -55,7 +55,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  console.log(req.method);
   if (req.method === 'POST') {
     let body = '';
 
